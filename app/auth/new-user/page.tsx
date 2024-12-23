770d2b46cf4b83eb8cf0e0bd4c3beadc78225e97
cristofer calcinas <cristofer.calcinas@uab.edu.bo>
2024-12-23 10:37:27 -0400

    refactor: mejora la estructura del componente de registro de usuario

--- app/auth/new-user/page.tsx
+++ app/auth/new-user/page.tsx
@@ -2,15 +2,14 @@ import { CreateUserForm } from "@/auth/register/create-user-form";
 
 export default function RegisterPage() {
   return (
-    <div className="w-full container mx-auto px-4 pt-16">
-      <h1 className="text-2xl font-bold text-center mb-6">
-        Registro de usuario
-      </h1>
-      <div className="max-w-[450px] mx-auto">
-        <section>
-          <CreateUserForm />
-        </section>
-      </div>
-    </div>
+    <main className="container mx-auto px-4 pt-16">
+      <article className="max-w-[450px] mx-auto">
+        <h1 className="text-2xl font-bold text-center mb-6">
+          Registro de usuario
+        </h1>
+        
+        <CreateUserForm />
+      </article>
+    </main>
   );
 }
