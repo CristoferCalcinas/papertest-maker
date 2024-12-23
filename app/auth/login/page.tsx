22f645de60ed1bc95d9764baf1e1a26f7eecbfea
cristofer calcinas <cristofer.calcinas@uab.edu.bo>
2024-12-23 10:35:18 -0400

    refactor: mejora la estructura del componente de inicio de sesión

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -2,24 +2,23 @@ import { EmailPasswordForm } from "@/auth/login/email-password-form";
 import { SocialAuthButtons } from "@/auth/login/social-auth-buttons";
 
 export default function LoginPage() {
-  return (
-    <div className="w-full container mx-auto px-4 pt-16">
-      <h1 className="text-2xl font-bold text-center mb-6">Inicio de sesión</h1>
-      <div className="max-w-[450px] mx-auto">
-        <section>
-          <SocialAuthButtons />
-        </section>
+ return (
+   <main className="container mx-auto px-4 pt-16">
+     <article className="max-w-[450px] mx-auto">
+       <h1 className="text-2xl font-bold text-center mb-6">
+         Inicio de sesión
+       </h1>
 
-        <div className="flex items-center my-8">
-          <div className="flex-grow h-px bg-gray-300"></div>
-          <span className="px-4 text-gray-500">o</span>
-          <div className="flex-grow h-px bg-gray-300"></div>
-        </div>
+       <SocialAuthButtons />
 
-        <section>
-          <EmailPasswordForm />
-        </section>
-      </div>
-    </div>
-  );
+       <div className="flex items-center my-8" role="separator">
+         <div className="flex-grow h-px bg-gray-300" />
+         <span className="px-4 text-gray-500">o</span>
+         <div className="flex-grow h-px bg-gray-300" />
+       </div>
+
+       <EmailPasswordForm />
+     </article>
+   </main>
+ );
 }
