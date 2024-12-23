01bec060c3b21617814e5f7fe383b40c4f5eec41
cristofer calcinas <cristofer.calcinas@uab.edu.bo>
2024-12-23 10:25:32 -0400

    feat: mejora el formulario de inicio de sesión con autocompletado y navegación

--- auth/login/email-password-form.tsx
+++ auth/login/email-password-form.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
@@ -15,9 +15,12 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+
 import { SignInSchema, signInSchema } from "../schemas/auth-schemas";
 
 export const EmailPasswordForm = () => {
+  const router = useRouter();
+
   const form = useForm<SignInSchema>({
     resolver: zodResolver(signInSchema),
     defaultValues: {
@@ -26,54 +29,56 @@ export const EmailPasswordForm = () => {
     },
   });
 
-  // 2. Define a submit handler.
+  // change function to handle-action
   function onSubmit(values: SignInSchema) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
     console.log({ values });
   }
+  
   return (
-    <div className="w-full">
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          {/* Email */}
-          <FormField
-            control={form.control}
-            name="email"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Correo electronico</FormLabel>
-                <FormControl>
-                  <Input
-                    type="email"
-                    placeholder="Ingrese su email"
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          {/* Password */}
-          <FormField
-            control={form.control}
-            name="password"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Contraseña</FormLabel>
-                <FormControl>
-                  <Input
-                    type="password"
-                    placeholder="Ingrese su contraseña"
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+        {/* Email */}
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Correo electronico</FormLabel>
+              <FormControl>
+                <Input
+                  type="email"
+                  placeholder="Ingrese su email"
+                  autoComplete="email"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
 
-          {/* Boton de inicio de sesion */}
+        {/* Password */}
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Contraseña</FormLabel>
+              <FormControl>
+                <Input
+                  type="password"
+                  placeholder="Ingrese su contraseña"
+                  autoComplete="current-password"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        {/* Boton de inicio de sesion */}
+        <div className="space-y-4">
           <Button className="w-full" type="submit">
             Iniciar sesión
           </Button>
@@ -83,14 +88,12 @@ export const EmailPasswordForm = () => {
             className="w-full"
             variant="outline"
             type="button"
-            onClick={() => {
-              redirect("/auth/register");
-            }}
+            onClick={() => router.push("/auth/new-user")}
           >
             Crear una cuenta
           </Button>
-        </form>
-      </Form>
-    </div>
+        </div>
+      </form>
+    </Form>
   );
 };
