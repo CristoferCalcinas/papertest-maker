fcb66ac09eb77a928109eb02abfa64bad3ee2596
cristofer calcinas <cristofer.calcinas@uab.edu.bo>
2024-12-24 15:46:16 -0400

    feat(auth): agregar soporte para JWT en la sesión y extender el tipo de sesión con id y roleId

--- auth/auth.ts
+++ auth/auth.ts
@@ -1,4 +1,4 @@
-import NextAuth from "next-auth";
+import NextAuth, { DefaultSession } from "next-auth";
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 // import Facebook from "next-auth/providers/facebook";
@@ -9,8 +9,47 @@ import { prisma } from "@/prisma";
 
 import { signInSchema } from "./schemas/auth-schemas";
 
+// ref: https://authjs.dev/getting-started/typescript
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+      roleId: string;
+    } & DefaultSession["user"];
+  }
+}
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    signIn: async ({ user, account, profile, email, credentials }) => {
+      // console.log({ user });
+      return true;
+    },
+    jwt: async ({ token, user, account, profile }) => {
+      // console.log({ token, user, account, profile, isNewUser });
+      const dbUser = await prisma.user.findUnique({
+        where: { email: token.email ?? "no-email" },
+      });
+      token.id = dbUser?.id ?? "no-uuid";
+      token.roleId = dbUser?.roleId ?? "no-role-id";
+
+      return token;
+    },
+    session: async ({ session, token, user }) => {
+      // console.log({ session, token, user });
+      console.log({ token });
+      if (session && session.user) {
+        session.user.id = token.id as string;
+        session.user.roleId = token.roleId as string;
+      }
+
+      return session;
+    },
+  },
   providers: [
     GitHub,
     Google,
