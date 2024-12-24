0983afffdbe6b4c98382ad753f1f69bab4882f8c
cristofer calcinas <cristofer.calcinas@uab.edu.bo>
2024-12-23 20:32:49 -0400

    feat(auth): agregar rutas personalizadas para inicio de sesión y nuevo usuario

--- auth/auth.ts
+++ auth/auth.ts
@@ -3,4 +3,8 @@ import GitHub from "next-auth/providers/github";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [GitHub],
+  pages: {
+    signIn: "/auth/login",
+    newUser: "/auth/new-user",
+  },
 });
