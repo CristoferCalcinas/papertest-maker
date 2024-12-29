c29b8ce4b071be7d8a9a94f4407d2708d9fb6c26
cristofer calcinas <cristofer.calcinas@uab.edu.bo>
2024-12-29 16:21:36 -0400

    feat(create-exam-page): mejorar la disposición del formulario y la vista previa para una mejor experiencia de usuario

--- app/exams/create/page.tsx
+++ app/exams/create/page.tsx
@@ -24,22 +24,29 @@ export default function CreateExamPage() {
       >
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="mx-auto grid max-w-2xl grid-cols-1 items-center gap-x-8 gap-y-16 sm:gap-y-24 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+            {/* Columna izquierda: Preview */}
             <article className="lg:pr-4 w-full h-full">
               <ExamPreview />
             </article>
 
-            <article>
-              <div className="text-base/7 text-gray-700 lg:max-w-lg">
-                <h1 className="text-base/7 font-semibold text-indigo-600">
-                  Nombre del Examen ?
-                </h1>
+            {/* Columna derecha: Formulario fijo */}
+            <div className="relative">
+              {/* Este div actúa como placeholder para mantener el espacio */}
+              <div className="h-[600px]" />
+              {/* Formulario fijo */}
+              <article className="fixed top-1/2 w-full max-w-lg -translate-y-1/2 p-2 border border-gray-200 rounded-lg bg-white shadow-sm">
+                <div className="text-base/7 text-gray-700">
+                  <h1 className="text-base/7 font-semibold text-indigo-600">
+                    Nombre del Examen ?
+                  </h1>
 
-                <div className="max-w-xl">
-                  <ExamForm />
-                  <ExamFormDivider />
+                  <div className="max-w-xl">
+                    <ExamForm />
+                    <ExamFormDivider />
+                  </div>
                 </div>
-              </div>
-            </article>
+              </article>
+            </div>
           </div>
         </div>
       </section>
