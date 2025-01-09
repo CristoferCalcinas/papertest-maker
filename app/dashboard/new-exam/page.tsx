4a1597b34a2224583c638d82c51773f4772fcfe6
cristofer calcinas <cristofer.calcinas@uab.edu.bo>
2025-01-09 09:26:14 -0400

    chore(new-exam): ajustar el espaciado en la página de creación de exámenes

--- app/dashboard/new-exam/page.tsx
+++ app/dashboard/new-exam/page.tsx
@@ -2,7 +2,7 @@ import { ExamManagement } from "@/dashboard/new-exam/exam-management";
 
 export default function NewExamPage() {
   return (
-    <main className="container mx-auto py-10">
+    <main className="container mx-auto py-2">
       <h1 className="text-3xl font-bold mb-8 text-center">
         Gestión un nuevo examen
       </h1>
